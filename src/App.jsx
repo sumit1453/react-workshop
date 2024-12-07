292b900e34fbdd26d912cadd5ad4a91c1ceb8c5b
sumit1453 <sumitnepali166@gmail.com>
2024-12-07 14:41:01 +0545

    React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { NavBar } from './components/NavBar'
+function App() {
+  return (<>
+    <NavBar />
+    <h1>Hello</h1>
+  </>)
+
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,3 @@
+export function NavBar() {
+    return <h1>Hello</h1>
+}
